b089e36f52f58f882c40fcf1cbcdd039a41c50f6
Aliaslol <isachenko.tolik@gmail.com>
2018-12-05 18:23:36 +0300

    fix 5

--- tasks/task5.groovy
+++ tasks/task5.groovy
@@ -2,11 +2,11 @@
 //@Grab(group='commons-validator', module='commons-validator', version='1.6'))
 //import org.apache.commons.validator.routines.InetAddressValidator
 
-def boolean isIp(String someIp) {
-	if(InetAddressValidator.getInstance().isValidInet4Address(someIp)){
-    	return true
-	} else {
-	    return false
-	}	
-}
-return this
+//def boolean isIp(String someIp) {
+//	if(InetAddressValidator.getInstance().isValidInet4Address(someIp)){
+//    	return true
+//	} else {
+//	    return false
+//	}	
+//}
+//return this
