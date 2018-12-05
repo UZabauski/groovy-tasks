8a7d418e3e8dcacf225242545ac2a0147bfa3b76
Aliaslol <isachenko.tolik@gmail.com>
2018-12-05 13:00:44 +0300

    task2

--- tasks/task2.groovy
+++ tasks/task2.groovy
@@ -1,6 +1,6 @@
 def mulEvenOdd(List<Integer> list) {
     list.eachWithIndex {it, index->
-        (it%2==0) ? list[index]=it*=2: list[index]=it*=3
+        (it%2==0) ? (list[index]=it*=2) : (list[index]=it*=3)
     }
     return list
 }
