f794e17f64841637cca62224e555bb185813e4e0
AndreiSkorkin <skorkin.andrei@gmail.com>
2018-12-07 03:37:27 -0500

    task2.1

--- tasks/task2.groovy
+++ tasks/task2.groovy
@@ -1,6 +1,5 @@
 def mulEvenOdd(List<Integer> evenodd) {
-  new_evenodd = evenodd.collect { (it%2==0) ? (it*2) : (it*3) } ; 
-    println(new_evenodd);
-}
+   println(evenodd.collect { (it%2==0) ? (it*2) : (it*3) } )  
+   }
 
 return this
