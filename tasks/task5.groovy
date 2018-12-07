946636641c19a3061ae80c2806824acba613460c
Jany22727 <RayJan@yandex.ru>
2018-12-07 09:07:00 +0000

    5th task 2nd vers

--- tasks/task5.groovy
+++ tasks/task5.groovy
@@ -2,7 +2,7 @@ def isIp(ip){
   c=0
   String[] octets = ip.split("\\.");
   octets.each{
-  if (Integer.parseInt(it)<=255 && Integer.parseInt(it)>=0 ) 
+  if (it.isNumber() && Integer.parseInt(it)<=255 && Integer.parseInt(it)>=0 ) 
     c+=1;
   }
   return c==4 && Integer.parseInt(octets[0])!=0
