2fb255ee117b79cff5bd46d53bb57e02ae3470e4
Jany22727 <RayJan@yandex.ru>
2018-12-07 04:58:39 +0000

    4th task

---
+++ tasks/task5.groovy
@@ -0,0 +1,10 @@
+def isIp(ip){
+  c=0
+  String[] octets = ip.split("\\.");
+  octets.each{
+  if (Integer.parseInt(it)<=255 && Integer.parseInt(it)>=0 ) 
+    c+=1;
+  }
+  return c==4 && Integer.parseInt(octets[0])!=0
+}
+return this
