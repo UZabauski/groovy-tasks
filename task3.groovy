37c93b9a2db8fbd36a2c6c2e955a3589e4011355
iharkazlouski <tamplierigor@gmail.com>
2018-12-09 21:31:40 +0300

    3.3

--- task3.groovy
+++ task3.groovy
@@ -1,7 +1,14 @@
-def isSublist(list1, list2) {
-  assert list1 instanceof java.util.list
-  assert list2 instanceof java.util.list
-  return  list1.containsAll(list2)
-}
+#def isSublist(list1, list2) {
+#  assert list1 instanceof java.util.list
+#  assert list2 instanceof java.util.list
+#  return  list1.containsAll(list2)
+#}
+#
+#return this
 
+
+def boolean isSublist(AbstractList<Integer> list1,list2) {
+    (list1.containsAll(list2)) ? true : false
+    
+}
 return this
