273c4b226824ecb24acf20b803ac3d30ac57c678
kkoles <ksup4ik@gmail.com>
2018-12-09 14:13:30 +0300

    Task 4 v.3

--- tasks/task4.groovy
+++ tasks/task4.groovy
@@ -2,7 +2,9 @@ def fib(n) {
     if (n>2) {
         return fib(n - 2) + fib(n - 1)
     }
-    else return 1
+    else if (n<=2 && n>=0) {return 1}
+    else { return println("Incorrect n") }
 }
 
 return this
+
