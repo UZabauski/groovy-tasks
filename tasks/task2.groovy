32968e4d7ee65fc65032a2aa5cc8d83ca75a64bd
murashandme <murashandme@gmail.com>
2018-12-09 14:13:53 +0300

    Revert "tasls 1234"
    This reverts commit 4cad4b6b558c5f3f151e871bf1cb60a73ba081b9.

--- tasks/task2.groovy
+++
@@ -1,7 +0,0 @@
-def mulEvenOdd(List<Integer> list){
-    def mulEvenOdd_list = []
-    list.each {(it%2==0) ? mulEvenOdd_list.add(it*2) : mulEvenOdd_list.add(it*3)}
-    return mulEvenOdd_list
-}
-
-return this
--- tasks/task3.groovy
+++
@@ -1,5 +0,0 @@
-boolean contains(List<?> list, List<?> sublist) {
-    return Collections.indexOfSubList(list, sublist) != -1;
-}
-
-return this
