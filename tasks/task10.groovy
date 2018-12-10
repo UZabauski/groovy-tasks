5194462b1ee52e97a1fe9c903fc2a08d465293e7
iharkazlouski <tamplierigor@gmail.com>
2018-12-10 13:19:06 +0300

    10-2

--- tasks/task10.groovy
+++ tasks/task10.groovy
@@ -1,6 +1,6 @@
 def urlText(a){
 	URL html = new URL(a)
 	URLConnection urlConnection = html.openConnection()
-	println "Response Code/Message: ${urlConnection.responseCode} / ${urlConnection.responseMessage}"
+	println "${urlConnection.responseCode} ${urlConnection.responseMessage}"
 }
 return this
