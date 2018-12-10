d7d96e34ef23f95f8abb1c10f729e58da2e30964
Anatol Isachanka <44891968+Aliaslol@users.noreply.github.com>
2018-12-10 12:22:11 +0300

    Update task8.groovy

--- tasks/task8.groovy
+++ tasks/task8.groovy
@@ -3,7 +3,6 @@ import groovy.json.JsonOutput
 
 def parseAndFilterJson(String myjson) {
   jsonSlurper = new JsonSlurper()
-  result =[:]
   def object = jsonSlurper.parseText(myjson)
   assert object instanceof Map
   return JsonOutput.toJson(object.findAll { it.value %9== 0 })
