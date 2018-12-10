3c18c682187977271f11e292d61e6007b502e3a0
Yauheniya Rayetskaya <44959828+Jany22727@users.noreply.github.com>
2018-12-10 10:25:13 +0300

    Update task8.groovy

--- tasks/task8.groovy
+++ tasks/task8.groovy
@@ -1,13 +1,13 @@
-import groovy.json.JsonSlurper
-import groovy.json.JsonOutput
+//import groovy.json.JsonSlurper
+//import groovy.json.JsonOutput
 
 def parseAndFilterJson(str){
-  jsonSlurper = new JsonSlurper()
-  json = jsonSlurper.parseText(str)
-  new_json= json.findAll{age->
-    age.value %9 == 0  
-  }
-  return JsonOutput.toJson(new_json)
+//  jsonSlurper = new JsonSlurper()
+//  json = jsonSlurper.parseText(str)
+//  new_json= json.findAll{age->
+//    age.value %9 == 0  
+//  }
+//  return JsonOutput.toJson(new_json)
 }
 return this
 
