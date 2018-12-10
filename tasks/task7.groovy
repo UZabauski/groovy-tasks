79b62e0ec737048d2e730180de3354fd736d4d60
Yauheniya Rayetskaya <44959828+Jany22727@users.noreply.github.com>
2018-12-10 10:08:35 +0300

    Update task7.groovy

--- tasks/task7.groovy
+++ tasks/task7.groovy
@@ -1,10 +1,10 @@
 def encryptThis(str){
-  String[] words = str.split(" ");
-  s=''
-  words.each{letter ->
-    new_word=((int)letter[0] + letter[-1] + letter[2..-2] + letter[1]+ ' ')
-    s+=new_word
-  }
-  return s
+//  String[] words = str.split(" ");
+//  s=''
+//  words.each{letter ->
+//    new_word=((int)letter[0] + letter[-1] + letter[2..-2] + letter[1]+ ' ')
+//    s+=new_word
+//  }
+//  return s
 }
 return this
