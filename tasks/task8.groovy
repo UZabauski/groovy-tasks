7a9a760f5e783e9f9bd6f5ff0287ae06b208d1f8
AndreiSkorkin <skorkin.andrei@gmail.com>
2018-12-11 07:04:44 -0500

    task8

---
+++ tasks/task8.groovy
@@ -0,0 +1,11 @@
+import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
+
+def parseAndFilterJson(json_in) {
+    def jsonSlurper = new JsonSlurper()
+    def parsed = jsonSlurper.parseText(json_in)
+    def result = parsed.findAll { it.value %9== 0 }
+    def json_out = JsonOutput.toJson(result)
+}
+
+return this
