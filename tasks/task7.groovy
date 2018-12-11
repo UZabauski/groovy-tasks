e216a63707776b029483aecc0d94b402e497e69b
uzabauski <uzabauski@gmail.com>
2018-12-11 14:56:16 +0300

    task7

---
+++ tasks/task7.groovy
@@ -0,0 +1,9 @@
+def encryptThis(String n){
+  def newb = n.split()
+  for(item in newb)
+    {
+      def encr=((int)item[0] + item[-1] + item[2..-2] + item[1]+ ' ')
+      print encr
+    }
+  }
+return this
