e4e6cb5c0753a0983104c39ac7902ae7cf15f06e
AndreiSkorkin <skorkin.andrei@gmail.com>
2018-12-11 09:11:38 -0500

    task7.1

--- tasks/task7.groovy
+++ tasks/task7.groovy
@@ -1,9 +1,11 @@
 def encryptThis(words){
   def items = words.split()
+  def space=''
   for(letter in items)
     {
       def encrypted=((int)letter[0] + letter[-1] + letter[2..-2] + letter[1] + ' ')
-      print(encrypted)
+      space+=encrypted
     }
+  return space.trim()
   }
 return this
